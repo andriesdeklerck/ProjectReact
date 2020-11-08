ad3835582b8feaf39801e462e411b5b5896d0bbf
Andries Deklerck <andriesdeklerck@hotmail.com>
2020-11-08 23:29:34 +0100

    Third commit

--- opdracht_react/src/index.js
+++ opdracht_react/src/index.js
@@ -32,13 +32,15 @@ class Name extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
-          First Name:
-          <input name="firstName" type="text" value={this.state.firstName} onChange={this.handleChange} />
-          Last Name:
-          <input name="lastName" type="text" value={this.state.lastName} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
+        <div class="form-group">
+          <h3 class="text-success">First Name:</h3>
+          <input name="firstName" class="form-control" type="text" value={this.state.firstName} onChange={this.handleChange} />
+        </div>
+        <div class="form-group">
+          <h3 class="text-success">Last Name:</h3>
+          <input name="lastName" class="form-control" type="text" value={this.state.lastName} onChange={this.handleChange} />
+        </div>
+        <input type="submit" class="btn btn-primary" value="Submit" />
       </form>
     );
   }
@@ -47,8 +49,7 @@ class Name extends React.Component {
 class SelectedButton extends React.Component {
   render() {
     return (
-      <button onClick={() => this.props.onClick()} >{PickedColor(this.props.value)}
-      </button>
+      <button onClick={() => this.props.onClick()} class="btn btn-lg m-1" style={{ background: PickedColor(this.props.value) }}></button>
     );
   }
 }
@@ -85,6 +86,21 @@ function PickedColor(i) {
   return kleur;
 }
 
+var tbl = document.createElement('table');
+
+function ready() {
+  tbl.setAttribute('class', 'table table-striped')
+  tbl.setAttribute('id', 'tabel');
+
+  var orderArrayHeader = ["Color 1", "Color 2", "Color 3", "Color 4", "Right color", "Right place"];
+  var thead = document.createElement('thead');
+  tbl.appendChild(thead);
+  for (var i = 0; i < orderArrayHeader.length; i++) {
+    thead.appendChild(document.createElement("th")).
+      appendChild(document.createTextNode(orderArrayHeader[i]));
+  }
+}
+
 class Game extends React.Component {
   constructor(props) {
     super(props);
@@ -92,12 +108,14 @@ class Game extends React.Component {
       colorsToChoose: Array(8).fill(null),
       makedGuess: Array(4),
       settedGame: Array(),
-      a: 0
+      a: 0,
+      tries: 0,
+      rightColor: 0,
+      rightPlace: 0
     };
   }
 
   handleClick(i) {
-    document.getElementById("gameColor").style.background = i;
     const selectedColor = this.state.colorsToChoose.slice();
     selectedColor[i] = PickedColor(i);
     this.setState({ colorsToChoose: selectedColor });
@@ -108,24 +126,68 @@ class Game extends React.Component {
   }
 
   setGame() {
-    this.state.settedGame = Array();
-    this.setState({
-      settedGame: this.state.settedGame.concat(this.state.makedGuess)
-    });
-    this.state.makedGuess = Array(4);
-    this.state.a = 0;
-    this.state.value = !this.state.value;
+    //document.getElementById("myList").innerHTML = "";
+    if (this.state.makedGuess[3] == null) {
+      alert("Select 4 colors");
+    } else {
+      this.state.settedGame = Array();
+      this.setState({
+        settedGame: this.state.settedGame.concat(this.state.makedGuess)
+      });
+      this.state.makedGuess = Array(4);
+      this.state.a = 0;
+      this.state.value = !this.state.value;
+      ready();
+    }
   }
 
   makeGuess() {
-    if (JSON.stringify(this.state.makedGuess) == JSON.stringify(this.state.settedGame)) {
-      this.state.value = !this.state.value;
-      this.state.a = 0;
-      alert('You have won: ' + this.state.makedGuess + " " + this.state.settedGame);
-    }
-    else {
-      this.state.makedGuess = Array(4);
-      this.state.a = 0;
+    if (this.state.makedGuess[3] == null) {
+      alert("Select 4 colors");
+    } else {
+
+      this.state.makedGuess.forEach(element => {
+        if (this.state.settedGame.includes(element)) {
+          this.state.rightColor += 1;
+        }
+      })
+
+      for (let i = 0; i < 4; i++) {
+        if (this.state.makedGuess[i] == this.state.settedGame[i]) {
+          this.state.rightPlace += 1;
+        }
+      }
+
+      for (var i = 0; i < 1; i++) {
+        var tr = tbl.insertRow();
+        for (var j = 0; j < 4; j++) {
+          var td0 = tr.insertCell();
+          td0.appendChild(document.createTextNode(this.state.makedGuess[j]));
+        }
+        var td0 = tr.insertCell();
+        td0.appendChild(document.createTextNode(this.state.rightColor - this.state.rightPlace));
+        td0 = tr.insertCell();
+        td0.appendChild(document.createTextNode(this.state.rightPlace));
+      }
+      document.getElementById("myList").appendChild(tbl);
+
+      this.setState({
+        tries: this.state.tries += 1,
+        rightColor: 0,
+        rightPlace: 0,
+        a: 0
+      });
+
+      if (JSON.stringify(this.state.makedGuess) === JSON.stringify(this.state.settedGame)) {
+        this.state.value = !this.state.value;
+        alert('You have won: ' + this.state.makedGuess + " " + this.state.settedGame + " with " + this.state.tries + " tries.");
+        this.state.tries = 0;
+        this.state.rightPlace = 0;
+        document.getElementById("tabel").innerHTML = "";
+      }
+      else {
+        this.state.makedGuess = Array(4);
+      }
     }
   }
 
@@ -142,75 +204,33 @@ class Game extends React.Component {
   render() {
     return (
       <div>
-        {this.renderButton(0)}
-        {this.renderButton(1)}
-        {this.renderButton(2)}
-        {this.renderButton(3)}
-        {this.renderButton(4)}
-        {this.renderButton(5)}
-        {this.renderButton(6)}
-        {this.renderButton(7)}
-        {/* <AddToBoard value={this.props.selectedColor} /> */}
-        <div id="gameColor">Selected color</div>
-        <button onClick={() => this.setGame()} disabled={this.state.value}>Set Color</button>
-        <button onClick={() => this.makeGuess()}>Make Guess</button>
+        <div>
+          <div>
+            {this.renderButton(0)}
+            {this.renderButton(1)}
+            {this.renderButton(2)}
+            {this.renderButton(3)}
+            {this.renderButton(4)}
+            {this.renderButton(5)}
+            {this.renderButton(6)}
+            {this.renderButton(7)}
+          </div>
+          <button class="btn btn-success m-1 w-25" onClick={() => this.setGame()} disabled={this.state.value}>Set Color</button>
+          <button class="btn btn-success m-1 w-25" onClick={() => this.makeGuess()}>Guess</button>
+        </div>
       </div>
     );
   }
 }
 
-// ReactDOM.render(
-//   <Name />,
-//   document.getElementById('user')
-// );
+ReactDOM.render(
+  <Name />,
+  document.getElementById('user')
+);
 
 ReactDOM.render(
   <Game />,
   document.getElementById('game')
-);
-
-class AddItem extends React.Component {
-  constructor() {
-    super();
-    this.state = { value: '' };
-    this.onChange = this.onChange.bind(this);
-    this.add = this.add.bind(this);
-  }
-
-  add() {
-    this.props.onButtonClick(this.state.value);
-    this.setState({ value: '' });
-  }
-
-  onChange(e) {
-    this.setState({ value: e.target.value });
-  }
-
-  render() {
-    return (
-      <div className="add-item">
-        <input
-          type="text"
-          className="add-item__input"
-          value={this.state.value}
-          onChange={this.onChange}
-          placeholder={this.props.placeholder}
-        />
-        <button
-          disabled={!this.state.value}
-          className="add-item__button"
-          onClick={this.add}
-        >
-          Add
-        </button>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(
-  <AddItem placeholder="Value" onButtonClick={v => console.log(v)} />,
-  document.getElementById('View')
 );
 
 // If you want to start measuring performance in your app, pass a function
